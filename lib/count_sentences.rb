495c4e3515ca156c36c48f8b78299acd17eae349
Alex Kitelinger <bureaucratix@gmail.com>
2019-04-23 14:39:05 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    punctuation = ['.', '?', '!']
+    split_string = split(Regexp.union(punctuation))
+    split_string.delete('')
+    split_string.length
   end
 end
